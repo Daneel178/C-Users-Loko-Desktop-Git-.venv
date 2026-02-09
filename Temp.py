c7910afe50542f41338e868a2d31b6c23bfeb8d8
Daneel178 <daneelkolosov2007@gmail.com>
2026-02-10 01:25:07 +0300

    Add files via upload

--- Temp.py
+++ Temp.py
@@ -1,20 +1,24 @@
 def what_to_wear():
-    temperature = float(input("Введите температуру: "))
-    if temperature < 0:
-        print("Пуховик")
-    elif temperature < 20:
-        print("Пальто")
-    elif temperature < 30:
+    temperature = input("Температура выше 20 градусов и меньше 30? (yes/no): ")
+    if temperature == "yes":
         is_rain = input("Есть осадки? (yes/no): ")
-        if is_rain == "yes":
-            is_strong_rain = input("Осадки сильные? (yes/no): ")
-            if is_strong_rain == "yes":
-                print("Пальто, резиновые сапоги и зонты")
-            else:
-                print("Пальто и дождевик")
-        else:
+        if is_rain == "no":
             print("Футболку и шорты")
+        else:
+            print("Футболку, шорты, дождевик")
     else:
-        print("Футболку и шорты")
+        temperature = input("Температура выше 0 градусов? (yes/no): ")
+        if temperature == "no":
+            print("Пуховик")
+        else:
+            is_rain = input("Есть осадки? (yes/no): ")
+            if is_rain == "no":
+                print("Пальто")
+            else:
+                is_strong_rain = input("Осадки сильные? (yes/no): ")
+                if is_strong_rain == "no":
+                    print("Пальто и дождевик")
+                else:
+                    print("Пальто, резиновые сапоги и зонт")
 
 what_to_wear()
